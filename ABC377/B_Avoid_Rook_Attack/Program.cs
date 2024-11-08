95a712bf1971b2a40f8977555b4dd2b5269a7cea
YUITY <make0up2ytm1nd9@gmail.com>
2024-11-08 23:43:57 +0900

    ABC377_D問題解答作成

--- ABC377/B_Avoid_Rook_Attack/Program.cs
+++ ABC377/B_Avoid_Rook_Attack/Program.cs
@@ -1,5 +1,33 @@
 ﻿using System;
 
+/*
+ <問題>
+ 縦 8 マス、横 8 マスの 64 マスからなるマス目があります。 上から i 行目 (1 ≤ i ≤ 8) 、左から j 列目 (1 ≤ j ≤ 8) のマスを
+ マス (i, j) と呼ぶことにします。
+ それぞれのマスは、空マスであるかコマが置かれているかのどちらかです。
+ マスの状態は長さ 8 の文字列からなる長さ 8 の列 (S(1), S(2), S(3), …, S(8)) で表されます。
+​ マス (i, j) (1 ≤ i ≤ 8, 1 ≤ j ≤ 8) は、S(i) の j 文字目が . のとき空マスで、# のときコマが置かれています。
+ あなたは、すでに置かれているどのコマにも取られないように、いずれかの空マスに自分のコマを置きたいです。
+ マス (i, j) に置かれているコマは、次のどちらかの条件を満たすコマを取ることができます。
+
+ ・i 行目のマスに置かれている
+ ・j 列目のマスに置かれている
+
+ あなたがコマを置くことができるマスがいくつあるか求めてください。
+
+ <制約>
+ ・S(i)は ., # からなる長さ 8 の文字列 (1 ≤ i ≤ 8)
+
+ <入力>
+ S(1)
+ S(2)
+ …
+ S(8)
+
+ <出力>
+ すでに置かれているコマに取られずに自分のコマを置くことができる空マスの個数を出力せよ。
+ */
+
 namespace B_Avoid_Rook_Attack {
     internal class Program {
         static void Main(string[] args) {
@@ -9,37 +37,35 @@ namespace B_Avoid_Rook_Attack {
             for (int i = 0; i < 8; i++) {
                 var wLine = Console.ReadLine();
                 for (int j = 0; j < 8; j++) {
-                    if (wLine[j] == '#') wBoard[i, j] = true;
+                    if (wLine[j] == '.') wBoard[i, j] = true;
                 }
             }
 
-            // 1行すべてfalseになっている行数
-            int wRowCount = 0;
-            for (int i = 0; i < 8; i++) {
-                bool wFlag = false;
-                for (int j = 0; j < 8; j++) {
-                    if (wBoard[i, j] == true) {
-                        wFlag = true;
+            // 出力
+            Console.WriteLine(CountAllTrueLine(wBoard, true) * CountAllTrueLine(wBoard, false));
+        }
+
+        /// <summary>
+        /// 1 行もしくは 1 列すべてが true となっている行数もしくは列数を返す
+        /// </summary>
+        /// <param name="vField">フィールド</param>
+        /// <param name="vIsRow">行をカウントする場合はtrue, 列をカウントする場合は false</param>
+        /// <returns>コマが全く置かれていない行数もしくは列数</returns>
+        static int CountAllTrueLine(bool[,] vField, bool vIsRow) {
+            int wLineCount = 0;
+            int wFirstLoopCount = vField.GetLength(vIsRow ? 0 : 1);
+            int wSecondLoopCount = vField.GetLength(vIsRow ? 1 : 0);
+            for (int i = 0; i < wFirstLoopCount; i++) {
+                bool wIsAllTrueLine = true;
+                for (int j = 0; j < wSecondLoopCount; j++) {
+                    if (!(vIsRow ? vField[i, j] : vField[j, i])) {
+                        wIsAllTrueLine = false;
                         break;
                     }
                 }
-                if (!wFlag) wRowCount++;
+                if (wIsAllTrueLine) wLineCount++;
             }
-
-            // 1列すべてfalseになっている列数
-            int wColumnCount = 0;
-            for (int i = 0; i < 8; i++) {
-                bool wFlag = false;
-                for (int j = 0; j < 8; j++) {
-                    if (wBoard[j, i] == true) {
-                        wFlag = true;
-                        break;
-                    }
-                }
-                if (!wFlag) wColumnCount++;
-            }
-
-            Console.WriteLine(wRowCount * wColumnCount);
+            return wLineCount;
         }
     }
 }
--- ABC377/C_Avoid_Knight_Attack/Program.cs
+++ ABC377/C_Avoid_Knight_Attack/Program.cs
@@ -1,116 +1,76 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-
-//namespace C_Avoid_Knight_Attack {
-//    internal class Program {
-//        static void Main(string[] args) {
-
-//            // 入力受付
-//            var wInput = Console.ReadLine().Split(' ');
-//            int wMatrixOrder = int.Parse(wInput[0]);
-//            int wPieceCount = int.Parse(wInput[1]);
-
-//            var wPiecies = new List<int[]>();
-//            for (int i = 0; i < wPieceCount; i++) {
-//                var wLine = Console.ReadLine().Split(' ');
-//                wPiecies.Add(new int[] { int.Parse(wLine[0]) - 1, int.Parse(wLine[1]) - 1 });
-//            }
-
-//            var wBoard = new bool[wMatrixOrder, wMatrixOrder];
-
-//            foreach (var wPiece in wPiecies) {
-//                // 駒があるところを true に変える
-//                wBoard[wPiece[0], wPiece[1]] = true;
-
-//                TurnOverBoard(wBoard, wPiece[0], wPiece[1]);
-//            }
-
-//            long wCount = 0;
-//            foreach (var wPosition in wBoard) {
-//                if (!wPosition) wCount++;
-//            }
-
-//            Console.WriteLine(wCount);
-//        }
-
-//        static void TurnOverBoard(bool[,] vBoard, int vRow, int vColumn) {
-//            int wRowCount = vBoard.GetLength(0);
-//            int wColumnCount = vBoard.GetLength(1);
-
-//            if (vRow - 1 >= 0 && vColumn - 2 >= 0) {
-//                vBoard[vRow - 1, vColumn - 2] = true;
-//            }
-//            if (vRow - 2 >= 0 && vColumn - 1 >= 0) {
-//                vBoard[vRow - 2, vColumn - 1] = true;
-//            }
-//            if (vRow - 2 >= 0 && vColumn + 1 < wColumnCount) {
-//                vBoard[vRow - 2, vColumn + 1] = true;
-//            }
-//            if (vRow - 1 >= 0 && vColumn + 2 < wColumnCount) {
-//                vBoard[vRow - 1, vColumn + 2] = true;
-//            }
-//            if (vRow + 1 < wRowCount && vColumn + 2 < wColumnCount) {
-//                vBoard[vRow + 1, vColumn + 2] = true;
-//            }
-//            if (vRow + 2 < wRowCount && vColumn + 1 < wColumnCount) {
-//                vBoard[vRow + 2, vColumn + 1] = true;
-//            }
-//            if (vRow + 2 < wRowCount && vColumn - 1 >= 0) {
-//                vBoard[vRow + 2, vColumn - 1] = true;
-//            }
-//            if (vRow + 1 < wRowCount && vColumn - 2 >= 0) {
-//                vBoard[vRow + 1, vColumn - 2] = true;
-//            }
-//        }
-//    }
-//}
-using System;
+﻿using System;
 using System.Collections.Generic;
+
+/*
+ <問題>
+ 縦 N マス、横 N マスの N^2 マスからなるマス目があります。 上から i 行目 (1 ≤ i ≤ N) 、
+ 左から j 列目 (1 ≤ j ≤ N) のマスをマス (i, j) と呼ぶことにします。
+ それぞれのマスは、空マスであるかコマが置かれているかのどちらかです。
+ マス目には合計で M 個のコマが置かれており、k 番目 (1 ≤ k ≤ M) のコマはマス (a(k), b(k)) に置かれています。
+ あなたは、すでに置かれているどのコマにも取られないように、いずれかの空マスに自分のコマを置きたいです。
+ マス (i, j) に置かれているコマは、次のどれかの条件を満たすコマを取ることができます。
+
+ ・マス (i + 2, j + 1) に置かれている
+ ・マス (i + 1, j + 2) に置かれている
+ ・マス (i − 1, j + 2) に置かれている
+ ・マス (i − 2, j + 1) に置かれている
+ ・マス (i − 2, j − 1) に置かれている
+ ・マス (i − 1, j − 2) に置かれている
+ ・マス (i + 1, j − 2) に置かれている
+ ・マス (i + 2, j − 1) に置かれている
+
+ ただし、存在しないマスについての条件は常に満たされないものとします。
+ あなたがコマを置くことができるマスがいくつあるか求めてください。
+
+ <制約>
+ ・1 ≤ N ≤ 10^9 
+ ・1 ≤ M ≤ 2×10^5
+ ・1 ≤ a(k) ≤ N, 1 ≤ b(k) ≤ N (1 ≤ k ≤ M)
+ ・(a(k), b(k)) != (a(l), b(l)) (1 ≤ k < l ≤ M)
+ ・入力はすべて整数
+
+ <入力>
+ N M
+ a(1) b(1)
+ a(2) b(2)
+ …
+ a(M) b(M)
+
+ <出力>
+ すでに置かれているコマに取られずに自分のコマを置くことができる空マスの個数を出力せよ。
+ */
 
 namespace C_Avoid_Knight_Attack {
     internal class Program {
-        static void Main(string[] args) {
-            // 入力受付
-            var wInput = Console.ReadLine().Split(' ');
-            int wMatrixOrder = int.Parse(wInput[0]);
-            int wPieceCount = int.Parse(wInput[1]);
 
-            // 駒の位置をリストに格納
-            var wPieces = new List<(int, int)>();
-            for (int i = 0; i < wPieceCount; i++) {
+        static readonly (int X, int Y)[] FDirection = { (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2) };
+        static void Main(string[] args) {
+
+            // 入力受付
+            var wInputLine = Console.ReadLine().Split(' ');
+            (long wMatrixOrder, int wPiecesCount) = (long.Parse(wInputLine[0]), int.Parse(wInputLine[1]));
+
+            var wFilledCells = new HashSet<(int X, int Y)>();
+            var wPieces = new List<(int X, int Y)>();
+            for (int i = 0; i < wPiecesCount; i++) {
                 var wLine = Console.ReadLine().Split(' ');
+                wFilledCells.Add((int.Parse(wLine[0]) - 1, int.Parse(wLine[1]) - 1));
                 wPieces.Add((int.Parse(wLine[0]) - 1, int.Parse(wLine[1]) - 1));
             }
 
-            // 影響を受けたマスを記録するハッシュセット
-            var attackedPositions = new HashSet<(int, int)>();
-
-            // 駒ごとに影響範囲を記録
-            foreach (var (row, col) in wPieces) {
-                attackedPositions.Add((row, col));  // 駒の位置を記録
-                MarkAttackedPositions(attackedPositions, row, col, wMatrixOrder);
-            }
-
-            // 全体の空いているマス数を計算
-            long wCount = (long)wMatrixOrder * wMatrixOrder - attackedPositions.Count;
-            Console.WriteLine(wCount);
-        }
-
-        static void MarkAttackedPositions(HashSet<(int, int)> attackedPositions, int row, int col, int size) {
-            // ナイトの移動パターン
-            int[] rowMoves = { -1, -2, -2, -1, 1, 2, 2, 1 };
-            int[] colMoves = { -2, -1, 1, 2, 2, 1, -1, -2 };
-
-            for (int i = 0; i < rowMoves.Length; i++) {
-                int newRow = row + rowMoves[i];
-                int newCol = col + colMoves[i];
-
-                // ボードの範囲内なら影響範囲に追加
-                if (newRow >= 0 && newRow < size && newCol >= 0 && newCol < size) {
-                    attackedPositions.Add((newRow, newCol));
+            // 各駒が移動できる座標を HashSet に追加する
+            foreach ((int X, int Y) wPiece in wPieces) {
+                foreach ((int X, int Y) in FDirection) {
+                    int wXCoordinate = wPiece.X + X;
+                    int wYCoordinate = wPiece.Y + Y;
+                    if (0 <= wXCoordinate && wXCoordinate < wMatrixOrder && 0 <= wYCoordinate && wYCoordinate < wMatrixOrder) {
+                        wFilledCells.Add((wXCoordinate, wYCoordinate));
+                    }
                 }
             }
+
+            // 出力
+            Console.WriteLine(wMatrixOrder * wMatrixOrder - (long)wFilledCells.Count);
         }
     }
 }
