cd8402ac317884acb9eea02e0a8da5aa46e99a3b
YUITY <make0up2ytm1nd9@gmail.com>
2024-11-30 20:55:37 +0900

    ABC380解答作成

---
+++ ABC380/C_Move_Segment/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ <問題>
+ 0, 1 のみからなる長さ N の文字列 S が与えられます。
+ S の中で先頭から K 番目の 1 の塊を K − 1 番目の 1 の塊の直後まで移動した文字列を出力してください。
+ なお、S には 1 の塊が K 個以上含まれることが保証されます。
+
+ より正確な説明は以下の通りです。
+ ・S の l 文字目から r 文字目までからなる部分文字列を S(l…r) と表す。
+ ・S の部分文字列 S(l…r) が 1 の塊であるとは、以下の条件を全て満たすことと定める。
+    ・S(l) = S(l + 1) = … = S(r) = 1 
+    ・l = 1 または S(l - 1) = 0 
+    ・r = N または S(r + 1) = 0 
+ ・S に含まれる 1 の塊が S(l(1)…r(1)), …, S(l(m)…r(m)) で全てであり、l(1) < … < l(m) を満たしているとする。 
+   このとき、以下で定義される長さ N の文字列 T を、「 S の中で先頭から K 番目の 1 の塊を K − 1 番目の 1 の塊の直後まで
+   移動した文字列 」と定める
+    ・1 ≤ i ≤ r(K − 1) のとき T(i) = S(i)
+    ・r(K − 1) + 1 ≤ i ≤ r(K − 1) + (r(K) − l(K)) + 1 のとき T(i) = 1
+    ・r(K − 1) + (r(K) - l(K)) + 2 ≤ i ≤ r(K) のとき T(i) = 0
+    ・r(K) + 1 ≤ i ≤ N のとき T(i) = S(i)
+
+ <制約>
+ ・1 ≤ N ≤ 5×10^5
+ ・S は 0, 1 のみからなる長さ N の文字列
+ ・2 ≤ K
+ ・S には 1 の塊が K 個以上含まれる
+
+ <入力>
+ N K
+ S
+
+ <出力>
+ 答えを出力せよ。
+ */
+
+namespace C_Move_Segment {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // 入力受付
+            var wTargetIndex = int.Parse(Console.ReadLine().Split(' ')[1]);
+            var wInputText = Console.ReadLine() + "X";  // 番兵 X を設定
+
+            var wSegments = new List<string>();
+            int wSegmentLength = 0;
+            int wStartIndex = 0;
+            for (int i = 0; i < wInputText.Length - 1; i++) {
+                wSegmentLength++;
+                if (wInputText[i] == wInputText[i + 1]) continue;
+
+                wSegments.Add(wInputText.Substring(wStartIndex, wSegmentLength));
+                wSegmentLength = 0;
+                wStartIndex = i + 1;
+            }
+
+            SwapSegments(wSegments, wTargetIndex);
+
+            // 出力
+            Console.WriteLine(string.Join("", wSegments));
+        }
+
+        /// <summary>
+        /// 部分文字列の順番を入れ替える
+        /// </summary>
+        /// <param name="vSegments">部分文字列のコレクション</param>
+        /// <param name="vTargetIndex">入れ替える 1 の部分文字列が何番目か</param>
+        static void SwapSegments(IList<string> vSegments, int vTargetIndex) {
+            if (vSegments[0][0] == '1') {
+                vTargetIndex = vTargetIndex * 2 - 2;
+            } else {
+                vTargetIndex = vTargetIndex * 2 - 1;
+            }
+            vSegments.Insert(vTargetIndex - 1, vSegments[vTargetIndex]);
+            vSegments.RemoveAt(vTargetIndex + 1);
+        }
+    }
+}
+
---
+++ ABC380/D_Strange_Mirroring/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ <問題>
+ 英大小文字からなる文字列 S が与えられます。
+ S に以下の操作を 10^100 回繰り返します。
+
+ ・まず、S の大文字を小文字に、小文字を大文字に書き換えた文字列を T とする。
+ ・その後、S と T とをこの順に連結した文字列を新たな S とする。
+
+ Q 個の質問に答えて下さい。 そのうち i 個目は次の通りです。
+
+ ・全ての操作を終えた後の S の先頭から K(i) 文字目を求めよ。
+
+ <制約>
+ ・S は英大小文字からなる長さ 1 以上 2×10^5 以下の文字列
+ ・Q, K(i) は整数
+ ・1 ≤ Q ≤ 2×10^5  
+ ・1 ≤ K(i) ≤ 10^18
+
+ <入力>
+ S Q
+ K(1) K(2) … K(Q)
+​
+ <出力>
+ i 個目の質問の答えを C(i) とする時、以下の形式で 1 行に空白区切りで出力せよ。
+ C(1) C(2) … C(Q)
+ */
+
+namespace D_Strange_Mirroring {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // 入力受付
+            string wAlphabets = Console.ReadLine();
+            int wQueryCount = int.Parse(Console.ReadLine());
+            var wQueries = Console.ReadLine().Split(' ').Select(x => long.Parse(x));
+
+            // 出力
+            var wOutputLine = new StringBuilder();
+            foreach (var wChar in ExecuteQueries(wAlphabets, wQueries)) {
+                wOutputLine.Append(wChar + " ");
+            }
+            wOutputLine.Length--;
+            Console.WriteLine(wOutputLine.ToString());
+        }
+
+        /// <summary>
+        /// 元の文字列とクエリを受け取り、クエリ実行結果を返す。
+        /// </summary>
+        /// <param name="vOriginal">元の文字列</param>
+        /// <param name="vQueries">クエリ</param>
+        /// <returns>クエリ実行により得られた文字</returns>
+        static IEnumerable<char> ExecuteQueries(string vOriginal, IEnumerable<long> vQueries) {
+
+            foreach (long wQuery in vQueries) {
+
+                // 指数
+                int wIndex = 0;
+                while ((wQuery / vOriginal.Length) > Math.Pow(2, wIndex)) {
+                    wIndex++;
+                }
+
+                long wSegmentOrder = 0;
+                int wAlphabetOrder = 0;
+                if (wQuery % vOriginal.Length == 0) {
+                    wSegmentOrder = wQuery / vOriginal.Length - 1;
+                    wAlphabetOrder = vOriginal.Length - 1;
+                } else {
+                    wSegmentOrder = wQuery / vOriginal.Length;
+                    wAlphabetOrder = (int)(wQuery % vOriginal.Length - 1);
+                }
+
+                yield return IsObverse(wSegmentOrder, wIndex) ? vOriginal[wAlphabetOrder] : GetInvertedChar(vOriginal[wAlphabetOrder]);
+            }
+        }
+
+        /// <summary>
+        /// 大文字 ⇔ 小文字 反転前の文字列であるか否かを返す。
+        /// </summary>
+        /// <param name="vSegmentOrder">文字列のまとまりの順番</param>
+        /// <param name="vIndex"></param>
+        /// <returns>反転前の文字列である場合true, そうではない場合false</returns>
+        static bool IsObverse(long vSegmentOrder, int vIndex) {
+            int wCount = 0;
+            while (vIndex >= 0) {
+                if (vSegmentOrder == 0) return wCount % 2 == 0;
+                if (vSegmentOrder == 1) break;
+                vSegmentOrder -= (long)Math.Pow(2, vIndex);
+                vIndex--;
+                wCount++;
+            }
+            return wCount % 2 == 1;
+        }
+
+        /// <summary>
+        /// アルファベットの 小文字 ⇔ 大文字 変換を行う
+        /// </summary>
+        /// <param name="vAlphabet">変換対象のアルファベット</param>
+        /// <returns>変換後のアルファベット</returns>
+        static char GetInvertedChar(char vAlphabet) => char.IsUpper(vAlphabet) ? char.ToLower(vAlphabet) : char.ToUpper(vAlphabet);
+    }
+}
