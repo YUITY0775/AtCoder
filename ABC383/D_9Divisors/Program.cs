0153415c73d51345386049dc8a0afb45234c6c22
YUITY <make0up2ytm1nd9@gmail.com>
2024-12-08 21:47:26 +0900

    ABC383解答作成

---
+++ ABC383/D_9Divisors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ <問題>
+ N 以下の正整数のうち、正の約数をちょうど 9 個持つものの個数を求めてください。
+
+ <制約>
+ ・1 ≤ N ≤ 4×10^12
+ ・入力される数値は全て整数
+
+ <入力>
+ N
+
+ <出力>
+ 答えを出力せよ。
+ */
+
+namespace D_9Divisors {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // 制の約数が 9 個ある = 素因数分解した時の結果が 〇^8 もしくは □^2 × △^2
+
+            long wLimitNumber = long.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
